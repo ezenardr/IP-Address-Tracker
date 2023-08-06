44d06955bd71d5bb8eba1c8b0ef03550e1e98ed6
ezenardr <ezenardr.dev@gmail.com>
2023-08-06 12:05:26 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# IP address tracker solution by ezenardr
+
+This is a solution to the [IP address tracker challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0).
+
+## Table of contents
+
+-   [Overview](#overview)
+    -   [The challenge](#the-challenge)
+    -   [Screenshot](#screenshot)
+    -   [Links](#links)
+-   [My process](#my-process)
+    -   [Built with](#built-with)
+-   [Author](#author)
+
+**Note: Delete this note and update the table of contents based on what sections you keep.**
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+-   View the optimal layout for each page depending on their device's screen size
+-   See hover states for all interactive elements on the page
+-   See their own IP address on the map on the initial page load
+-   Search for any IP addresses or domains and see the key information and location
+
+### Screenshot
+
+![](./design/desktop-design.jpg)
+![](./design/mobile-design.jpg)
+
+### Links
+
+-   Live Site URL: [https://ezenardr-Ip-Address-Tracker.netlify.app](https://ezenardr-Ip-Address-Tracker.netlify.app)
+
+## My process
+
+### Built with
+
+-   Semantic HTML5 markup
+-   CSS custom properties
+-   Flexbox
+-   CSS Grid
+-   Mobile-first workflow
+-   [React](https://reactjs.org/) - JS library
+-   [Vitejs](https://nextjs.org/) -
+-   [TypeScript](https://www.typescriptlang.org/)
+-   [CSS Modules](https://github.com/css-modules/css-modules) - For styles
+
+## Author
+
+-   Portfolio - [ezenardr.com](https://www.ezenardr.com)
+-   Github - [ezenardr](https://www.github.com/ezenardr)
+-   Frontend Mentor - [@ezenardr](https://www.frontendmentor.io/profile/ezenardr)
+-   Twitter - [@ezenardr.dev](https://www.twitter.com/ezenardr.dev)
---
+++ src/App.tsx
@@ -0,0 +1,132 @@
+import { useState, useEffect } from 'react';
+import { IoIosArrowForward } from 'react-icons/io';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import Styles from './App.module.css';
+
+interface Location {
+    ip: string;
+    isp: string;
+    location: {
+        city: string;
+        region: string;
+        timezone: string;
+        lat: number;
+        lng: number;
+    };
+}
+type LocationType = Location | undefined;
+const App = () => {
+    const [location, setLocation] = useState<LocationType>(undefined);
+    const [query, setQuery] = useState<string>('');
+    const [error, setError] = useState<string | null>(null);
+    useEffect(() => {
+        async function fetchData() {
+            setError(null);
+            const data = await fetch(
+                `https://geo.ipify.org/api/v2/country,city?apiKey=at_ywehIVtFVxwkmx0ZH6Dtqi2bx9lU1&ipAddress=`
+            );
+            const jsonData = await data.json();
+            setLocation(jsonData);
+        }
+        fetchData();
+    }, []);
+    const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const ipTest =
+            /((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9a-f]{1,4}:){7}([0-9a-f]{1,4}|:))|(([0-9a-f]{1,4}:){6}(:[0-9a-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9a-f]{1,4}:){5}(((:[0-9a-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9a-f]{1,4}:){4}(((:[0-9a-f]{1,4}){1,3})|((:[0-9a-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9a-f]{1,4}:){3}(((:[0-9a-f]{1,4}){1,4})|((:[0-9a-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9a-f]{1,4}:){2}(((:[0-9a-f]{1,4}){1,5})|((:[0-9a-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9a-f]{1,4}:){1}(((:[0-9a-f]{1,4}){1,6})|((:[0-9a-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9a-f]{1,4}){1,7})|((:[0-9a-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))/gi;
+        // const ipTest =
+        //     /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gi;
+        if (query.length > 4) {
+            try {
+                ipTest.test(query);
+            } catch (error) {
+                setError('Enter a valid IP Address');
+            }
+            const data = await fetch(
+                `https://geo.ipify.org/api/v2/country,city?apiKey=at_ywehIVtFVxwkmx0ZH6Dtqi2bx9lU1&ipAddress=${query}`
+            );
+            const jsonData = await data.json();
+            setLocation(jsonData);
+            setQuery('');
+        }
+    };
+    return (
+        <>
+            <header>
+                <h1 className={Styles.title}>IP Address Tracker</h1>
+                <form onSubmit={(e) => submitHandler(e)}>
+                    <input
+                        type="text"
+                        onChange={(e) => setQuery(e.target.value)}
+                        placeholder="Search for any IP address or domain"
+                        value={query}
+                    />
+                    <IoIosArrowForward
+                        onClick={submitHandler}
+                        className={Styles.icon}
+                    />
+                </form>
+                {error && <p>{error}</p>}
+                {location && (
+                    <div className={Styles.data}>
+                        <div
+                            className={`${Styles.dataBox} ${Styles.borderRight}`}
+                        >
+                            <p className={Styles.dataBoxTitle}>IP address</p>
+                            <p className={Styles.dataData}>{location.ip}</p>
+                        </div>
+                        <div
+                            className={`${Styles.dataBox} ${Styles.borderRight}`}
+                        >
+                            <p className={Styles.dataBoxTitle}>location</p>
+                            <p className={Styles.dataData}>
+                                {location?.location?.region}
+                            </p>
+                        </div>
+                        <div
+                            className={`${Styles.dataBox} ${Styles.borderRight}`}
+                        >
+                            <p className={Styles.dataBoxTitle}>timezone</p>
+                            <p className={Styles.dataData}>
+                                UTC {` ${location.location?.timezone}`}
+                            </p>
+                        </div>
+                        <div className={`${Styles.dataBox}`}>
+                            <p className={Styles.dataBoxTitle}>isp</p>
+                            <p className={Styles.dataData}>{location.isp}</p>
+                        </div>
+                    </div>
+                )}
+            </header>
+            {location && (
+                <div className={Styles.map}>
+                    <MapContainer
+                        center={[
+                            location.location?.lat,
+                            location.location?.lng,
+                        ]}
+                        zoom={15}
+                        scrollWheelZoom={false}
+                        style={{ width: '100vw', height: '100vh' }}
+                    >
+                        <TileLayer
+                            noWrap={true}
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker
+                            position={[
+                                location.location?.lat,
+                                location.location?.lng,
+                            ]}
+                        >
+                            <Popup>{location.location?.city}</Popup>
+                        </Marker>
+                    </MapContainer>
+                </div>
+            )}
+        </>
+    );
+};
+
+export default App;
