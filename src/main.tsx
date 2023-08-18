6c339ac4c26ea83cdb227dab415ac5ce0c23bfbc
ezenardr <ezenardr.dev@gmail.com>
2023-08-18 13:22:32 -0400

    Fixed root typescript issue in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const element = document.getElementById('root')!;
+ReactDOM.createRoot(element).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
